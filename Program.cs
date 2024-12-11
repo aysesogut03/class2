cd830a08d396dc6002960e0245f8fe709ad6886d
aysesogut03 <aysesogut2003@gmail.com>
2024-12-11 09:22:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            araba araba1 = new araba();
+            araba1.arabaModel = "Renault";
+            araba1.arabaRengi = "Kırmızı";
+            araba1.kapiSayisi = 4;
+            araba1.kapilarKilitle();
+            araba1.mototCalistir();
+
+            Console.WriteLine("***********");
+            Console.WriteLine("Araba modeli:" + araba1.arabaModel);
+            Console.WriteLine("Araba rengi:" + araba1.arabaRengi);
+            Console.WriteLine("Kapı sayısı:" + araba1.kapiSayisi);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ araba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    public class araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+
+        public void mototCalistir()
+        {
+            Console.WriteLine("Motor çalışıyor...");
+        }
+
+        public void kapilarKilitle()
+        {
+            Console.WriteLine("Kapılar kilitleniyor.");
+        }
+    }
+}
